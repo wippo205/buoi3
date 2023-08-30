b2ca329dcc5b0612e85565de3e8f0f36ec414eaa
NguyenHuuDai <h1h1macon@gmail.com>
2023-08-30 16:01:21 +0700

    Them Tam Giac

---
+++ buoi3/HinhTamGiac.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi3
+{
+
+
+class HinhTamGiac
+{
+    private double canhA;
+    private double canhB;
+    private double canhC;
+
+    public HinhTamGiac()
+    {
+        canhA = 0;
+        canhB = 0;
+        canhC = 0;
+    }
+
+    public HinhTamGiac(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0 || !LaTamGiac(a, b, c))
+        {
+            canhA = 0;
+            canhB = 0;
+            canhC = 0;
+        }
+        else
+        {
+            canhA = a;
+            canhB = b;
+            canhC = c;
+        }
+    }
+
+    public double CanhA
+    {
+        get { return canhA; }
+        set { if (value > 0 && LaTamGiac(value, canhB, canhC)) canhA = value; }
+    }
+
+    public double CanhB
+    {
+        get { return canhB; }
+        set { if (value > 0 && LaTamGiac(canhA, value, canhC)) canhB = value; }
+    }
+
+    public double CanhC
+    {
+        get { return canhC; }
+        set { if (value > 0 && LaTamGiac(canhA, canhB, value)) canhC = value; }
+    }
+
+    public double TinhChuVi()
+    {
+        return CanhA + CanhB + CanhC;
+    }
+
+    public double TinhDienTich()
+    {
+        double p = TinhChuVi() / 2;
+        return Math.Sqrt(p * (p - CanhA) * (p - CanhB) * (p - CanhC));
+    }
+
+    public string KieuTamGiac()
+    {
+        if (CanhA == CanhB && CanhB == CanhC)
+            return "Đeu";
+        if (CanhA == CanhB || CanhA == CanhC || CanhB == CanhC)
+            return "Can";
+        return "Thuong";
+    }
+
+    private bool LaTamGiac(double a, double b, double c)
+    {
+        return a + b > c && a + c > b && b + c > a;
+    }
+
+    public override string ToString()
+    {
+        string kieu = KieuTamGiac();
+        return "  " + CanhA + "         " + CanhB + "         " + CanhC + "          " + kieu + "             " + TinhChuVi() + "             " + TinhDienTich();
+    }
+}
+
+
+}
--- buoi3/Program.cs
+++ buoi3/Program.cs
@@ -8,31 +8,46 @@ namespace buoi3
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+            
+        //    SinhVien sv1 = new SinhVien(2001207444, "Nguyen Huu Dai", -1, -2);
+        //    Console.WriteLine(sv1);
+
+        //    if (sv1.KiemTraHopLe())
+        //        Console.WriteLine("Sinh viên hop ke");
+        //    else
+        //        Console.WriteLine("Sinh vien khong hop le");
+
+        //    Console.Write("\n \n \n \n");
+
+        //    SinhVien sv2 = new SinhVien(2001207445, "Nguyen Huu Dat", 1, 2);
+        //    Console.WriteLine(sv2);
+
+        //    if (sv2.KiemTraHopLe())
+        //        Console.WriteLine("Sinh viên hop ke");
+        //    else
+        //        Console.WriteLine("Sinh vien khong hop le");
+        //    Console.ReadLine();
+
+        //    Console.Write("\n \n \n \n"); 
+        //}
+
         static void Main(string[] args)
         {
-            
-            SinhVien sv1 = new SinhVien(2001207444, "Nguyen Huu Dai", -1, -2);
-            Console.WriteLine(sv1);
+            HinhTamGiac tg1 = new HinhTamGiac(4, 5, 6);
+            HinhTamGiac tg2 = new HinhTamGiac(3, 8, 10);
+            HinhTamGiac tg3 = new HinhTamGiac(5, 5, 5);
+            HinhTamGiac tg4 = new HinhTamGiac(6, 6, 8);
+            HinhTamGiac tg5 = new HinhTamGiac(3, 4, 5);
 
-            if (sv1.KiemTraHopLe())
-                Console.WriteLine("Sinh viên hop ke");
-            else
-                Console.WriteLine("Sinh vien khong hop le");
-
-            Console.Write("\n \n \n \n");
-
-            SinhVien sv2 = new SinhVien(2001207445, "Nguyen Huu Dat", 1, 2);
-            Console.WriteLine(sv2);
-
-            if (sv2.KiemTraHopLe())
-                Console.WriteLine("Sinh viên hop ke");
-            else
-                Console.WriteLine("Sinh vien khong hop le");
+            Console.WriteLine("Canh A    Canh B    Canh C         Kieu            Chu Vi             Dien Tich \n");
+            Console.WriteLine(tg1.ToString());
+            Console.WriteLine(tg2.ToString());
+            Console.WriteLine(tg3.ToString());
+            Console.WriteLine(tg4.ToString());
+            Console.WriteLine(tg5.ToString());
             Console.ReadLine();
-
-            Console.Write("\n \n \n \n");
-
-          
         }
     }
 }
